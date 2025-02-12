f9d5e9fa0a43958cb6005e22323e2c390b5b34f1
marissa <mar_ath@hotmail.gr>
2025-02-12 10:15:32 +0200

    About Section ready

--- src/layouts/About.jsx
+++ src/layouts/About.jsx
@@ -1,5 +1,34 @@
+import React from "react";
+import "../styles/About.css";
+import About1 from "../assets/About1.jpg";
+import About2 from "../assets/About2.jpg";
+
 function About() {
-  return <div>Some text goes here</div>;
+  return (
+    <section className="about">
+      <div className="about-text">
+        <h2>Little Lemon Restaurant</h2>
+        <h3>Chicago</h3>
+        <p>
+          At Little Lemon, we bring the heart and soul of Mediterranean cuisine
+          to your table with fresh, locally sourced ingredients and timeless
+          recipes. Nestled in the heart of the community, our family-owned
+          restaurant is a celebration of bold flavors, warm hospitality, and a
+          passion for authentic cooking. Whether you're here for a casual lunch,
+          a cozy dinner, or a special occasion, we invite you to savor our
+          handcrafted dishes inspired by the sun-kissed coasts of the
+          Mediterranean. From zesty citrus-infused specialties to traditional
+          favorites, every bite is a journey of taste and tradition. Join us at
+          Little Lemon, where every meal is made with love and served with a
+          smile!
+        </p>
+      </div>
+      <div className="img-container">
+        <img className="Img1" src={About1} alt="About1" />
+        <img className="Img2" src={About2} alt="About2" />
+      </div>
+    </section>
+  );
 }
 
 export default About;
